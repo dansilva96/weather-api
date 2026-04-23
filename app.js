3dcf5c4020a6b7cc8e4ba54aa770f33d0b872d28
dansilva96 <dan.silva963@gmail.com>
2026-04-23 09:44:01 -0300

    organize folder structure

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
-const limiter = require('./rateLimiter')
-const { connectRedis } = require('./redisClient')
+const limiter = require('./src/rateLimiter')
+const { connectRedis } = require('./src/redisClient')
 const cors = require('cors')
 const app = express()
 
-const weatherController = require('./weatherController')
+const weatherController = require('./src/weatherController')
 
 app.use(express.json())
 app.use(cors())
@@ -18,6 +18,6 @@ const port = process.env.PORT || 3000
 
 connectRedis().then(() => {
     app.listen(port, () => {
-        console.log(`Servidor rodando em http://localhost:${port}`)
+        console.log(`Servidor rodando em http://localhost:${port}/weather`)
     })
 })
